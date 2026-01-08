0fce0a43eca33bf03935f8cc5b9522566bd9ad13
dang-hoanghai1209 <34885445+dang-hoanghai1209@users.noreply.github.com>
2026-01-08 22:09:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cartRoutes = require("./routes/cartRoutes");
 const checkoutRoutes = require("./routes/checkoutRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const publicDir = __dirname;
 
